1db98a0dbe1ed3357e171d89738313b708f0553f
John Rees <john@bitsushi.com>
2014-11-22 11:45:35 +0100

    readings

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,8 +1,7 @@
 class Reading < ActiveRecord::Base
-  belongs_to :station
+  belongs_to :station, touch: true
 
   default_scope -> { order('recorded_at DESC') }
-
 
   def to_s
     "[Bikes: #{bikes}, Slots: #{slots}, at: #{created_at.stamp('15:33:22 30/10')}]"

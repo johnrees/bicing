5b163c46834cad1efa27795f6e3acd8a2b5eeb41
John Rees <john@bitsushi.com>
2014-11-25 11:41:43 +0100

    group

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -6,7 +6,8 @@ class StationsController < ApplicationController
   end
 
   def map
-    @stations = Station.eager_load(:readings).where("readings.recorded_at <= ?", (params[:datetime] ? (params[:datetime].to_datetime - 1.hour) : Time.now)).order("readings.recorded_at DESC")
+    # @readings = Reading.maximum("recorded_at").where("readings.recorded_at <= ?", (params[:datetime] ? (params[:datetime].to_datetime - 1.hour) : Time.now)).order("readings.recorded_at DESC")
+    @stations = Station.eager_load(:readings).where("readings.recorded_at <= ?", (params[:datetime] ? (params[:datetime].to_datetime - 1.hour) : Time.now)).group('readings.id').order("readings.recorded_at DESC")
     @reading = @stations.first.latest_reading
     render :map, layout: false
   end
